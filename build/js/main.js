d5f36604141d38136b57541f4936450ca9c355e8
shayan heidary <hdrydeveloper@gmail.com>
2023-10-12 11:08:12 +0200

    basic types

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
-let userName = "sha";
-console.log(userName);
+let myName = "Shayan";
+let meaningOFLife;
+let isLoading;
+let album;
+myName = "23";
+meaningOFLife = 27;
+isLoading = true;
+album = 10;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
-let userName = "sha";
-console.log(userName);
+let myName: string = "Shayan";
+let meaningOFLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "23";
+meaningOFLife = 27;
+isLoading = true;
+album = 10;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
